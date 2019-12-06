1b60128b1764f614ca6f9497e7fcc10ec0430275
unknown <bertram_e@ymail.com>
2019-12-06 13:39:03 -0600

    Updates

---
+++ precache-manifest.1f2f2a8d85def22c6d60aa7d42cc38e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d342406a42d8068767e5fa042f065372",
+    "url": "/robofriends2/index.html"
+  },
+  {
+    "revision": "fff6bcfce69b7a3b063e",
+    "url": "/robofriends2/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "555832ac7670000e9dcc",
+    "url": "/robofriends2/static/css/main.536e06a6.chunk.css"
+  },
+  {
+    "revision": "fff6bcfce69b7a3b063e",
+    "url": "/robofriends2/static/js/2.fb398b8b.chunk.js"
+  },
+  {
+    "revision": "555832ac7670000e9dcc",
+    "url": "/robofriends2/static/js/main.dbaa0050.chunk.js"
+  },
+  {
+    "revision": "a8e9305f613d4f34bbd5",
+    "url": "/robofriends2/static/js/runtime-main.8a618456.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends2/static/media/SEGA.12a3db8b.woff"
+  }
+]);
